0d1cbaf60250bf1ca8e4a48042cce73ff0e79808
Miso <un5754@naver.com>
2024-01-04 15:53:34 +0900

    feat: 댓글 응답 데이터 클래스

--- src/main/kotlin/com/teamsparta/todo/domain/comment/dto/CommentResponse.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/comment/dto/CommentResponse.kt
@@ -2,6 +2,6 @@ package com.teamsparta.todo.domain.comment.dto
 
 data class CommentResponse(
     val id: Long,
-    val userName: String,
+    val commentUserName: String,
     val content: String,
 )
