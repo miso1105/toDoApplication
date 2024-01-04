e68480d9606910b15fe2ef974cd96704128ecf4f
Miso <un5754@naver.com>
2024-01-04 15:54:06 +0900

    feat: 할일 작성 요청 데이터 클래스

--- src/main/kotlin/com/teamsparta/todo/domain/todo/dto/CreateTodoRequest.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/todo/dto/CreateTodoRequest.kt
@@ -1,5 +1,7 @@
 package com.teamsparta.todo.domain.todo.dto
 
+import com.teamsparta.todo.domain.todo.model.DoneStatus
+
 data class CreateTodoRequest(
     val userName: String,
     val title: String,
