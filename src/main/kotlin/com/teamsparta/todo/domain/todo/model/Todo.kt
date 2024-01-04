0d2b96b38b23a29966087a08c4cfc0fa486b7a95
Miso <un5754@naver.com>
2024-01-04 15:58:49 +0900

    feat: 할일 완료 엔티티 매핑과 댓글 기능 추가

--- src/main/kotlin/com/teamsparta/todo/domain/todo/model/Todo.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/todo/model/Todo.kt
@@ -1,5 +1,6 @@
 package com.teamsparta.todo.domain.todo.model
 
+import com.teamsparta.todo.domain.comment.model.Comment
 import com.teamsparta.todo.domain.todo.dto.TodoResponse
 import jakarta.persistence.*
 import java.time.LocalDateTime
@@ -19,12 +20,31 @@ class Todo(
     @Column(name = "plans")
     var plans: String? = null,
 
+    @Enumerated(EnumType.STRING)
+    @Column
+    var doneStatus: DoneStatus,
+
+    @OneToMany(mappedBy = "todo", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
+    var comments: MutableList<Comment> = mutableListOf()
+
 ) {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
+    fun addComment(comment: Comment) {
+        comments.add(comment)
+    }
+
+    fun removeComment(comment: Comment) {
+        comments.remove(comment)
+    }
+
+    // 할일 완료하면 호출되는 메소드
+    fun updateDoneStatus() {
+        doneStatus = DoneStatus.TRUE
+    }
 
 }
 
@@ -35,5 +55,6 @@ fun Todo.toResponse(): TodoResponse {
         title = title,
         plans = plans,
         createdDate = LocalDateTime.now(),
+        doneStatus = doneStatus.name,
     )
 }
