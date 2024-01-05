6503c68caf17447372789d4085455828a94e1821
Miso <un5754@naver.com>
2024-01-05 12:17:07 +0900

    fix: (Spring) Ambiguous handler methods mapped for HTTP path 오류 수정

--- src/main/kotlin/com/teamsparta/todo/domain/todo/repository/TodoRepository.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/todo/repository/TodoRepository.kt
@@ -4,4 +4,4 @@ import com.teamsparta.todo.domain.todo.model.Todo
 import org.springframework.data.jpa.repository.JpaRepository
 
 
-interface TodoRepository: JpaRepository<Todo, Long>
+interface TodoRepository: JpaRepository<Todo, Long>    // 컨트롤러 - 서비스 - 레포지토리가 결합돼 자동으로 인스턴스 하나가 생성돼 기능들이 실행 가능한 것
