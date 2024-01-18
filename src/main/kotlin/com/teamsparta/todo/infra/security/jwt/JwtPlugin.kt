69d0555111cba70d7424adb24c995d61c259dc5e
Miso <un5754@naver.com>
2024-01-18 12:55:14 +0900

    feat: JWT 토큰 생성을 위한 claims 커스텀, 토큰 생성, 필터에 쓰일 토큰 검증 메서드 생성
    generateToken() 의 custom claim 으로 email 과 role 을 받고 토큰 만료시간 expirationPeriod = 168 시간 생성, key 는 Keys.hmacShaKeyFor() 을 사용해 시크릿 키 객체를 생성
    
    시큐리티 필터에 쓰일 validateToken() 메서드의 파라미터에 여기서 생성한 jwt 토큰을 파라미터로 받아서 payload 에 들어갈 커스텀한 claims 를 JWS 에 담아 서명한 토큰을 Result 의 객체로 받아 다음 통신인 JwtAuthenticationFilter 로 전달한다. kotlin.runCatching 으로 토큰 유효 검사의 성공이나 실패일 때 다른 리턴을 받을 수 있다.

---
+++ src/main/kotlin/com/teamsparta/todo/infra/security/jwt/JwtPlugin.kt
@@ -0,0 +1,61 @@
+package com.teamsparta.todo.infra.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+
+@Component
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long,
+) {
+
+
+    // 검증 parsing
+    fun validateToken(jwt: String): Result<Jws<Claims>> {
+        return kotlin.runCatching {
+            val key =
+                Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
+        }
+    }
+
+    fun generateAccessToken(
+        subject: String,
+        email: String,
+        role: String
+    ): String {
+        return generateToken(subject, email, role, Duration.ofHours(accessTokenExpirationHour))
+
+    }
+
+    private fun generateToken(subject: String, email: String, role: String, expirationPeriod: Duration): String {
+        val claims: Claims = Jwts.claims()
+            .add(mapOf("role" to role, "email" to email))
+            .build()
+
+        val now = Instant.now()
+        val expirationPeriod = Duration.ofHours(168)
+        val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+
+
+        return Jwts.builder()
+            .subject(subject)
+            .issuer(issuer)
+            .issuedAt(Date.from(now))  // 토큰 발행 시간, Instant는 인간이 읽을 수 없는 시간의 정수표기법
+            .expiration(Date.from(now.plus(expirationPeriod)))    // 토큰 만료 시간
+            .claims(claims)
+            .signWith(key)
+            .compact()
+    }
+
+
+}
