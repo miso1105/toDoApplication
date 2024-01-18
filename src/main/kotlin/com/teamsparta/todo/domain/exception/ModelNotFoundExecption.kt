6b34c34a80a49aed8bc8c79fa26f5dd8685f9653
Miso <un5754@naver.com>
2024-01-18 19:55:57 +0900

    변동사항 없음

--- src/main/kotlin/com/teamsparta/todo/domain/exception/ModelNotFoundExecption.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/exception/ModelNotFoundExecption.kt
@@ -2,6 +2,6 @@ package com.teamsparta.todo.domain.exception
 
 
 
-data class ModelNotFoundException(val modelName: String, val id: Long): RuntimeException(         // RuntimeException => applicaton이 실행될 떄 발생되는 예외
-    "Model $modelName not found with given id: $id"                // Model card not found with given id: 1
+data class ModelNotFoundException(val modelName: String, val id: Long?): RuntimeException(
+    "Model $modelName not found with given id: $id"
 )
