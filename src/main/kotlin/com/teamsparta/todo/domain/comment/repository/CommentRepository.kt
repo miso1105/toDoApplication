a264b2321755c56d9d1c0f8b1f5c2fb3cdb7a7ee
Miso <un5754@naver.com>
2024-01-20 18:56:48 +0900

    feat: 인터페이스 CustomCommentRepository 작성
    JPAQueryFactory(entityManager) 추상클래스를 CustomCommentRepository 를 통해 상속받은 기존의 CommentRepository

--- src/main/kotlin/com/teamsparta/todo/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/comment/repository/CommentRepository.kt
@@ -3,4 +3,7 @@ package com.teamsparta.todo.domain.comment.repository
 import com.teamsparta.todo.domain.comment.model.Comment
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommentRepository: JpaRepository<Comment, Long>
+
+interface CommentRepository: JpaRepository<Comment, Long>, CustomCommentRepository {
+
+}
