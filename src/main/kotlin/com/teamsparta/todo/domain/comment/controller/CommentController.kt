d2ceb2aa9edb8c93205ea45e63de2639f8c13b81
Miso <un5754@naver.com>
2024-01-20 18:15:14 +0900

    feat: queryDsl을 사용한 댓글 리스트 조회 api 작성
    입력한 단어를 기준으로 댓글 작성자의 이름에 포함돼있다면 조회된다.

--- src/main/kotlin/com/teamsparta/todo/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/comment/controller/CommentController.kt
@@ -8,19 +8,24 @@ import com.teamsparta.todo.domain.todo.service.TodoService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/todos/{todoId}/comments")
 @RestController
 class CommentController(
     private val todoService: TodoService
 ) {
+
+    // 쿼리디에셀 사용 api
+    @GetMapping("/search")
+    @PreAuthorize("hasRole('MEMBER') or hasRole('ADMIN')")
+    fun searchCommentList(
+        @RequestParam(value = "commentUserName") commentUserName: String
+    ): ResponseEntity<List<CommentResponse>> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.searchCommentList(commentUserName))
+    }
 
 
     @PreAuthorize("hasRole('MEMBER') or hasRole('ADMIN')")
