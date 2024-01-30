6432972afd29278e349d3e20b78b393be8fadcbb
Miso <un5754@naver.com>
2024-01-30 22:54:44 +0900

    feat: request validation 추가

--- src/main/kotlin/com/teamsparta/todo/domain/comment/dto/CommentRequest.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/comment/dto/CommentRequest.kt
@@ -3,7 +3,7 @@ package com.teamsparta.todo.domain.comment.dto
 import jakarta.validation.constraints.NotBlank
 
 
-data class AddCommentRequest(
+data class CommentRequest(
     @field:NotBlank(message = "댓글 내용을 입력해주세요")
     val content: String,
 )
--- src/main/kotlin/com/teamsparta/todo/domain/user/dto/LoginRequest.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/user/dto/LoginRequest.kt
@@ -9,5 +9,4 @@ data class LoginRequest(
     val email: String,
     @field:NotBlank(message = "비밀번호를 입력해주세요")
     val password: String,
-
 )
