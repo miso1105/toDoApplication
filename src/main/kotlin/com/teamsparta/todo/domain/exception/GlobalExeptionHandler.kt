7c2398604ecce1885b0326cc2a0e291f8ff7e8ae
Miso <un5754@naver.com>
2024-01-30 23:01:00 +0900

    feat: MethodArgumentNotValidException 예외 작성

--- src/main/kotlin/com/teamsparta/todo/domain/exception/GlobalExeptionHandler.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/exception/GlobalExeptionHandler.kt
@@ -1,9 +1,9 @@
 package com.teamsparta.todo.domain.exception
 
 import com.teamsparta.todo.domain.exception.dto.ErrorResponse
-import com.teamsparta.todo.domain.user.exception.InvalidCredentialException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -38,4 +38,16 @@ class GlobalExceptionHandler {
             .body(ErrorResponse(message = e.message))
     }
 
+//    @ExceptionHandler(UnathorizedException::class)
+//    fun handleUnathorizedException(e: UnathorizedException): ResponseEntity<ErrorResponse> {
+//        return ResponseEntity
+//            .status(HttpStatus.UNAUTHORIZED)
+//            .body(ErrorResponse(message = e.message))
+//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse(e.bindingResult.allErrors.map { it.defaultMessage }.reduce { acc, cur -> "$acc, $cur"  }))
+    }
 }
